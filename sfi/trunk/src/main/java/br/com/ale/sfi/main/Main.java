a16ad6b421058f710739f0aca10d89b3e59740f7
e-candido <e-candido@a6ab9a00-9471-48e1-84f9-b43d928f8953>
2015-10-07 01:32:50 +0000

    Contrucao do Main.

--- sfi/trunk/src/main/java/br/com/ale/sfi/main/Main.java
+++ sfi/trunk/src/main/java/br/com/ale/sfi/main/Main.java
@@ -1,9 +1,31 @@
 package br.com.ale.sfi.main;
 
+import java.io.File;
+import java.util.List;
+
+import br.com.ale.sfi.config.Config;
+import br.com.ale.sfi.process.SalaryFileProcess;
+import br.com.ale.sfi.utils.FileUtil;
+
 public class Main {
 
-    public static void main(String[] args) {
+	private static final String FILE_EXTENSION = "csv";
 
-    }
+	public static void main(String[] args) {
+
+		Config config = Config.getInstance();
+		FileUtil fileUtil = FileUtil.getInstance();
+		List<File> listFiles = fileUtil.listFiles(config.getFilePathIn(), FILE_EXTENSION);
+		
+		if(!listFiles.isEmpty()){
+			File file = listFiles.get(0);
+			SalaryFileProcess salaryFileProcess = new SalaryFileProcess();
+			salaryFileProcess.process(file);
+		}
+		
+		
+		
+	
+	}
 
 }
