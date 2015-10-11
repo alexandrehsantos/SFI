a1ff85f5cf02d39db316afddbc139972ed7d1299
e-candido <e-candido@a6ab9a00-9471-48e1-84f9-b43d928f8953>
2015-10-11 20:39:29 +0000

    Implementação do gerador de arquivos de testes

---
+++ sfi/trunk/src/test/java/br/com/ale/sfi/testes/GeneratorFileTest.java
@@ -0,0 +1,49 @@
+package br.com.ale.sfi.testes;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.ale.sfi.exception.SFIException;
+
+public class GeneratorFileTest {
+
+	private static final int QTE_LINES = 1000 * 50;
+	private File file;
+
+	@Before
+	public void init() {
+		file = new File("/opt/sfi/files/in", "Salary.csv");
+	}
+
+	@Test
+	public void generete() {
+
+		try {
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true));
+			try {
+
+				for (int i = 0; i < QTE_LINES; i++) {
+
+					bufferedWriter.write("yearId-" + i + "," + "teamId-" + i
+							+ "," + "lgId-" + i + "," + "playerId-" + i + ","
+							+ "salary-" + i);
+					bufferedWriter.newLine();
+					bufferedWriter.flush();
+				}
+
+			} finally {
+				bufferedWriter.close();
+			}
+
+		} catch (IOException e) {
+			throw new SFIException("Erro ao gerar aquivo de teste", e);
+		}
+
+	}
+
+}
